4fd164fb1807914ac7f2982d51846cf65b887a7a
Nicolas Frankel <nicolas@frankel.ch>
2020-03-26 19:08:57 +0100

    Migrate the check of changed bytecode from hack to pipeline API

--- bytecode-stream/src/main/kotlin/org/hazelcast/bytecodestream/SourceBuilder.kt
+++ bytecode-stream/src/main/kotlin/org/hazelcast/bytecodestream/SourceBuilder.kt
@@ -1,14 +1,13 @@
 package org.hazelcast.bytecodestream
 
-import com.hazelcast.jet.core.Processor.Context
 import com.hazelcast.function.BiConsumerEx
 import com.hazelcast.function.FunctionEx
+import com.hazelcast.jet.core.Processor.Context
 import com.hazelcast.jet.pipeline.SourceBuilder
 import com.hazelcast.jet.pipeline.SourceBuilder.SourceBuffer
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
-import java.util.*
 
 fun classes() = SourceBuilder
     .stream("classes", TargetPathContext())
@@ -28,10 +27,7 @@ class ClassPathReader : BiConsumerEx<ContextHolder, SourceBuffer<Pair<String, By
         visitor.classes.forEach {
             val name = toClassName.applyEx(it)
             val content = Files.readAllBytes(it)
-            if (ctx.classes[name] == null || !content.contentEquals(ctx.classes[name]!!)) {
-                ctx.classes[name] = content
-                buffer.add(name to content)
-            }
+            buffer.add(name to content)
         }
     }
 }
@@ -46,6 +42,4 @@ class PathToClassName(private val root: Path) : FunctionEx<Path, String> {
 
 }
 
-class ContextHolder(val classesDirectory: Path = Paths.get(System.getProperty("target"))) {
-    var classes: MutableMap<String, ByteArray> = mutableMapOf()
-}
+class ContextHolder(val classesDirectory: Path = Paths.get(System.getProperty("target")))
