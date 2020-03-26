d9ec96b9abf28f9748728d074a9a45eca0d5107d
Nicolas Frankel <nicolas@frankel.ch>
2020-03-26 19:08:57 +0100

    Migrate the check of changed bytecode from hack to pipeline API

--- bytecode-stream/src/main/kotlin/org/hazelcast/bytecodestream/Pipeline.kt
+++ bytecode-stream/src/main/kotlin/org/hazelcast/bytecodestream/Pipeline.kt
@@ -1,13 +1,27 @@
 package org.hazelcast.bytecodestream
 
+import com.hazelcast.function.BiPredicateEx
 import com.hazelcast.jet.Util.entry
 import com.hazelcast.jet.pipeline.Pipeline
+import com.hazelcast.jet.pipeline.ServiceFactories
 import com.hazelcast.jet.pipeline.Sinks
+import com.hazelcast.map.IMap
 
 fun pipeline() = Pipeline.create().apply {
+    val mapName = "bytecode"
     readFrom(classes())
         .withIngestionTimestamps()
         .peek { it.first }
+        .filterUsingService(
+            ServiceFactories.iMapService(mapName),
+            CheckChange()
+        )
         .map { entry(it.first, it.second) }
-        .writeTo(Sinks.map("bytecode"))
+        .writeTo(Sinks.map(mapName))
+}
+
+class CheckChange : BiPredicateEx<IMap<String, ByteArray>, Pair<String, ByteArray>> {
+
+    override fun testEx(map: IMap<String, ByteArray>, pair: Pair<String, ByteArray>) =
+        map[pair.first] == null || !pair.second.contentEquals(map[pair.first]!!)
 }
